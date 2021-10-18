5bf5ed0ea7c7fa3268091178d9cd4e010ee17d47
ElBalerio <nicolasvalerio96@hotmail.com>
2021-10-17 21:15:03 -0300

    entrega4

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,12 @@
-//Función que se ejecuta una vez que se haya lanzado el evento de
-//que el documento se encuentra cargado, es decir, se encuentran todos los
-//elementos HTML presentes.
-document.addEventListener("DOMContentLoaded", function(e){
+var cartInfo = {};
 
+
+
+document.addEventListener("DOMContentLoaded", function(e){
+    getJSONData(REAL_CART_INFO_URL).then(function (resultOBJ) {
+        //si el status del json esta ok procede a realizar tareas
+        if (resultOBJ.status === "ok") {
+            cartInfo = resultOBJ.data;
+        }
+    });
 });
--- js/init.js
+++ js/init.js
@@ -5,6 +5,7 @@ const PRODUCTS_URL = "https://japdevdep.github.io/ecommerce-api/product/all.json
 const PRODUCT_INFO_URL = "https://japdevdep.github.io/ecommerce-api/product/5678.json";
 const PRODUCT_INFO_COMMENTS_URL = "https://japdevdep.github.io/ecommerce-api/product/5678-comments.json";
 const CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/987.json";
+const REAL_CART_INFO_URL = "https://japdevdep.github.io/ecommerce-api/cart/654.json";
 const CART_BUY_URL = "https://japdevdep.github.io/ecommerce-api/cart/buy.json";
 const OBT_NAV_VAR = document.getElementById('navbar');
 const OBT_LOCAL_STORAGE_USERNAME = localStorage.getItem('username');
@@ -64,13 +65,21 @@ function spawnNavBar() {
         <a class="nav-link" href="sell.html">Vender</a>
       </li>
       <li class="nav-item active">
-        <a class="nav-link" href="cart.html">Mi carrito</a>
-      </li>
-      <li class="nav-item active">
-      <input type="submit" value=` + OBT_LOCAL_STORAGE_USERNAME + ` class="nav-item py-2"  id="logout">
+        <div class="dropdown">
+          <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            ` + OBT_LOCAL_STORAGE_USERNAME + `
+          </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="./my-profile.html">Mi perfil</a>
+            <a class="dropdown-item" href="./cart.html">Ver mi carrito</a>
+            <a class="dropdown-item" id="logout" href="#">Cerrar sesión</a>
+          </div>
+        </div>
       </li>
     </ul>
     </div>`
+    // <input type="submit" value=` + OBT_LOCAL_STORAGE_USERNAME + ` class="nav-item py-2"  id="logout">
+
     // <div class="container d-flex flex-column flex-md-row justify-content-between collapse navbar-collapse" id="navbarSupportedContent">
     // <a class="py-2 d-none d-md-inline-block" href="inicio.html">Inicio</a>
     // <a class="py-2 d-none d-md-inline-block" href="categories.html">Categorías</a>
